6f2d1723787530856444f577a1e85822c913341c
Henrique Reis <henrique.reis90@outlook.com>
2026-04-12 13:31:48 -0300

    v1.0.0

--- main.tf
+++ main.tf
@@ -2,6 +2,77 @@ resource "aws_s3_bucket" "cloudtrail" {
   bucket = "${var.project_name}-cloudtrail-logs"
 }
 
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket_policy" "cloudtrail" {
+  bucket = aws_s3_bucket.cloudtrail.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AWSCloudTrailAclCheck"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+        Action   = "s3:GetBucketAcl"
+        Resource = aws_s3_bucket.cloudtrail.arn
+      },
+      {
+        Sid    = "AWSCloudTrailWrite"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+        Action = "s3:PutObject"
+        Resource = "${aws_s3_bucket.cloudtrail.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "cloudtrail_to_cw" {
+  name = "${var.project_name}-cloudtrail-to-cw-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "cloudtrail_to_cw" {
+  name = "${var.project_name}-cloudtrail-to-cw-policy"
+  role = aws_iam_role.cloudtrail_to_cw.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "${aws_cloudwatch_log_group.trail.arn}:*"
+      }
+    ]
+  })
+}
+
 resource "aws_cloudwatch_log_group" "trail" {
   name = "${var.project_name}-logs"
 }
@@ -10,8 +81,14 @@ resource "aws_cloudtrail" "main" {
   name                          = "${var.project_name}-trail"
   s3_bucket_name                = aws_s3_bucket.cloudtrail.id
   cloud_watch_logs_group_arn    = "${aws_cloudwatch_log_group.trail.arn}:*"
+  cloud_watch_logs_role_arn     = aws_iam_role.cloudtrail_to_cw.arn
   is_multi_region_trail         = true
   include_global_service_events = true
+
+  depends_on = [
+    aws_s3_bucket_policy.cloudtrail,
+    aws_iam_role_policy.cloudtrail_to_cw
+  ]
 }
 
 resource "aws_sns_topic" "alerts" {
